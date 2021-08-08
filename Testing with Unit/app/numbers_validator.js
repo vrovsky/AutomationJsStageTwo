25c2c7c8db794898b8033d4939f1b5fd4dca59e2
Viachaslau Rovsky <vrovsky@gmail.com>
2021-08-08 16:27:13 +0300

    Mocha tasks added

---
+++ Testing with Unit/app/numbers_validator.js
@@ -0,0 +1,54 @@
+/* eslint-disable quotes */
+/* eslint-disable indent */
+/**
+ *
+ * A simple calss containing metods for validating numbers
+ * @class NumbersValidator
+ */
+class NumbersValidator {
+  /**
+   * Creates an instance of NumbersValidator.
+   * @memberof NumbersValidator
+   */
+  constructor() {}
+  /**
+   *
+   *
+   * @param {Number} number number to check
+   * @return {Boolean} true if number is even
+   * @memberof NumbersValidator
+   */
+  isNumberEven(number) {
+    const typeOfVariable = typeof number;
+    // eslint-disable-next-line quotes
+    if (typeOfVariable !== 'number') {
+      throw new Error(
+        // eslint-disable-next-line comma-dangle
+        `${number} is not of type "Number" it is of type "${typeOfVariable}"`
+      );
+    } else {
+      return number % 2 === 0;
+    }
+  }
+
+  /**
+   *
+   *
+   * @param {Array<Number>} arrayOfNumbers array of numbers to go throught
+   * @return {Array<Number>} array of even nubmers
+   * @memberof NumbersValidator
+   */
+  evenNumbersFromArray(arrayOfNumbers) {
+    if (
+      Array.isArray(arrayOfNumbers) &&
+      arrayOfNumbers.every((item) => typeof item === 'number')
+    ) {
+      const arrayOfEvenNumbers = arrayOfNumbers.filter(this.isNumberEven);
+      return arrayOfEvenNumbers;
+    } else {
+      throw new Error(`[${arrayOfNumbers}] is not an array of "Numbers"`);
+    }
+  }
+}
+
+module.exports = NumbersValidator;
---
+++ Testing with Unit/test/specs/isNumberEven.spec.js
@@ -0,0 +1,19 @@
+/* eslint-disable object-curly-spacing */
+const NumbersValidator = require('../../app/numbers_validator');
+const { expect } = require('chai');
+
+describe('isNumberEven positive scenarios', function () {
+  let validator;
+  beforeEach(function () {
+    validator = new NumbersValidator();
+  });
+  afterEach(function () {
+    validator = null;
+  });
+  it(`should return true if number is even`, function () {
+    expect(validator.isNumberEven(4)).to.be.equal(true);
+  });
+  it(`should return false if number is odd`, function () {
+    expect(validator.isNumberEven(5)).to.be.equal(false);
+  });
+});
