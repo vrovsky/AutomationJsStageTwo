542c2d80866d04232544089923c1d092fdd2e5dc
Viachaslau Rovsky <vrovsky@gmail.com>
2021-08-09 22:03:36 +0300

    Unit testing task added

---
+++ unitTask/app/calculator.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * A simple calculator class containing add and multiply methods
+ * @class Calculator
+ */
+class Calculator {
+  /**
+   * Creates an instance of Calculator.
+   * @memberof Calculator
+   */
+  constructor() {}
+  /**
+   *
+   *
+   * @param {Number} firstNumber
+   * @param {Number} secondNumber
+   * @return {Number} sum of two numbers
+   * @memberof Calculator
+   */
+  add(firstNumber, secondNumber) {
+    const typeOfFirstNumber = typeof firstNumber;
+    const typeOfSecondNumber = typeof secondNumber;
+    if (typeOfFirstNumber !== 'number' && typeOfSecondNumber !== 'number') {
+      throw new Error(`You should use numbers to calculate them.`);
+    } else {
+      return firstNumber + secondNumber;
+    }
+  }
+  /**
+   *
+   *
+   * @param {Number} firstNumber
+   * @param {Number} secondNumber
+   * @return {Number} result of multiplying firstNumber to secondNumber
+   * @memberof Calculator
+   */
+  multiply(firstNumber, secondNumber) {
+    const typeOfFirstNumber = typeof firstNumber;
+    const typeOfSecondNumber = typeof secondNumber;
+    if (typeOfFirstNumber !== 'number' && typeOfSecondNumber !== 'number') {
+      throw new Error(`You should use numbers to calculate them.`);
+    } else {
+      return firstNumber * secondNumber;
+    }
+  }
+}
+
+module.exports = Calculator;
