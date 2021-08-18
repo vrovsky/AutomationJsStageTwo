d69b8bd78c8e8d6d74d2f9e00678ddb9591eb6a9
Viachaslau Rovsky <vrovsky@gmail.com>
2021-08-18 21:30:46 +0300

    simple test with ebay added

---
+++ WebDriverIO/test/specs/search.js
@@ -0,0 +1,26 @@
+describe('Ebay Product search', () => {
+  it('should open the main url and verify the title', () => {
+    browser.url('http://ebay.com');
+    expect(browser).toHaveTitle(
+      'Электроника, автомобили, мода, коллекционирование, купоны и другие товары | eBay'
+    );
+  });
+
+  it('should search for product and verify the search text value', async () => {
+    const searchInput = await $('#gh-ac');
+    const searchButton = await $('#gh-btn');
+    await searchInput.addValue('laptop');
+    await searchButton.click();
+    await expect(searchInput).toHaveValue('laptop');
+  });
+
+  it('should redirect to a new page and verify the title', async () => {
+    await expect(browser).toHaveTitle('laptop | eBay');
+  });
+
+  it('should update the search category', async () => {
+    const category = await $('#gh-cat option:nth-child(1)');
+
+    await expect(category).toHaveText('Ноутбуки и нетбуки PC');
+  });
+});
