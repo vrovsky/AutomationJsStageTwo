04dc379772dd47d1d74c5f621e6199983b965dcf
Viachaslau Rovsky <vrovsky@gmail.com>
2021-08-25 21:17:16 +0300

    changed spec param to cloudgoogle

--- WebDriverIO/Hurt Me Plenty/wdio.conf.js
+++ WebDriverIO/Hurt Me Plenty/wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ['./test/specs/**/pastebin.js'],
+  specs: ['./test/specs/**/cloudgoogle.js'],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
