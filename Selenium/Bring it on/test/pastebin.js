a146262931b3eb7edd5245dece01554eb01e2f46
Viachaslau Rovsky <vrovsky@gmail.com>
2021-08-30 21:54:05 +0300

    Bring it on Selenium task completed

--- Selenium/Bring it on/test/pastebin.js
+++ Selenium/Bring it on/test/pastebin.js
@@ -1,14 +1,15 @@
 const PastebinPage = require('../pageobject/pastebin.page');
 const { expect } = require('chai');
+const { By, until } = require('selenium-webdriver');
 
 describe('Bring it on task for selenium webdriver tutorial', function () {
   this.timeout(50000);
   before(() => {
     PastebinPage.open(PastebinPage.pastebinUrl);
   });
-  // after(() => {
-  //   driver.quit();
-  // });
+  after(() => {
+    driver.quit();
+  });
   it('should create new paste', async () => {
     await PastebinPage.enter_postform(PastebinPage.gitCommands);
     await PastebinPage.find_syntaxHighlighlitingForm();
@@ -16,10 +17,20 @@ describe('Bring it on task for selenium webdriver tutorial', function () {
     await PastebinPage.find_expirationForm();
     await PastebinPage.chose_expiration();
     await PastebinPage.enter_postformName(PastebinPage.title);
+    await driver.wait(
+      until.titleIs('how to gain dominance among developers - Pastebin.com')
+    );
   });
-  it('should check new paste fields', async () => {
-    await PastebinPage.check_title();
-    await PastebinPage.check_textHighlighter();
-    await PastebinPage.check_pasteText(PastebinPage.gitCommands);
+  it('should check bash syntax highlighter', async () => {
+    let bashSyntax = await driver
+      .findElement(By.xpath('//a[@href="/archive/bash"]'))
+      .getText();
+    expect(bashSyntax).to.equal('Bash');
+  });
+  it('should check pastecode', async () => {
+    let pasteCode = await driver
+      .findElement(By.xpath('//textarea[@class="textarea"]'))
+      .getText();
+    expect(pasteCode).to.equal(PastebinPage.gitCommands);
   });
 });
